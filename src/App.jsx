fe1860e138113c420e760c549c3d2427ca895455
SK0603 <khalid.hamid2003@gmail.com~>
2025-06-05 18:35:29 +0300

    fixed some deployement issues

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Dashboard from './pages/dashboard';
-import Patients from './pages/Patients';
+import Patients from './pages/patients';
 import Appointments from './pages/Appointments';
 import Records from './pages/records';
 import Login from './pages/Login';
